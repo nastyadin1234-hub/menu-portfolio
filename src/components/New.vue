773d130266a6cb2df4605c4189367bcd0c1406b0
nastyadin1234-hub <nastyadin1234@gmail.com>
2026-05-20 22:15:15 +0300

    Update New.vue

--- src/components/New.vue
+++ src/components/New.vue
@@ -116,7 +116,7 @@ import zyzhikImg from '../assets/IMG_20260504_154103.webp'
 import zyzhikcutImg from '../assets/Screenshot 2026-05-12 190038.jpg'
 import bombaImg from '../assets/IMG_20260504_153904.webp'
 import schtollenImg from '../assets/IMG_20260504_154230.webp'
-import macaronImg from '../assets/IMG_20260519_110512.webp'
+import macaronImg from './IMG_20260519_1105121.webp'
 import macaroncutImg from '../assets/IMG_20260519_110510.webp'
 
 const currentCategory = ref('desserts')
@@ -136,7 +136,7 @@ const allProducts = ref([
   },
   { 
     id: 2, 
-    title: 'Карамельные Макароны', 
+    title: 'Карамельные Макарон', 
     price: 'По запросу', 
     category: 'desserts', 
     images: [macaronImg, macaroncutImg], 
