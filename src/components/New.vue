604202e97c79256730dba0b931c5b403ec90d245
nastyadin1234-hub <nastyadin1234@gmail.com>
2026-07-19 12:33:25 +0300

    Update New.vue

--- src/components/New.vue
+++ src/components/New.vue
@@ -165,7 +165,6 @@ const allProducts = ref([
     category: 'desserts', 
     images: [korzinkaImg, korzinkacutImg], 
     currentImgIndex: 0,
-    badge: 'Новинка',
     weight: '8 см диаметр',
     desc: 'Песочное тесто с карамелью и орехом Макадамия, попкорн, карамельный мусс'
   },{ 
